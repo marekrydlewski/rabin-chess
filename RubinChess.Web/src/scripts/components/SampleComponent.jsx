62399d28d37c5dfac8636acd9fb19e97014f0f29
mpanowicz <michal.panowicz@student.put.poznan.pl>
2016-04-02 23:36:40 +0200

    Add some coments

--- RubinChess.Web/src/scripts/components/SampleComponent.jsx
+++ RubinChess.Web/src/scripts/components/SampleComponent.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
+/**
+ * SampleComponent class
+ */
 export default class SampleComponent extends React.Component {
+  /**
+   * Display div
+   * @return {div} Routed thing
+   */
   render() {
     return (
       <div>Routed thing</div>
