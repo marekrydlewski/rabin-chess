34844fd5f34325315c103932e88b4e88e186c487
Marek Rydlewski <marek.rydlewski@outlook.com>
2016-04-06 23:35:47 +0200

    Fix router && add somne dependencies

--- RabinChess.Web/src/scripts/main.jsx
+++ RabinChess.Web/src/scripts/main.jsx
@@ -2,13 +2,13 @@ import '../styles/style'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route } from 'react-router'
+import { Router, Route, hashHistory } from 'react-router'
 
 import App from './components/App.jsx'
 import SampleComponent from './components/SampleComponent.jsx'
 
 ReactDOM.render((
-  <Router>
+  <Router history={ hashHistory }>
     <Route path="/" component={ App } >
       <Route path="routed" component={ SampleComponent } />
     </Route>
