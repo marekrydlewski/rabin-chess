cf5b6266c9caabd12dc11a204b9a6e04f94bd1ed
Marek Rydlewski <marek.rydlewski@hotmail.com>
2016-04-07 15:22:19 +0200

    Fix imports

--- RabinChess.Web/src/scripts/components/SampleComponent.jsx
+++ RabinChess.Web/src/scripts/components/SampleComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import style from './sample_component.scss'
+import style from './sample_component'
 import {Grid, Row, Col} from 'react-flexbox-grid'
 /**
  * SampleComponent class
@@ -16,7 +16,7 @@ export default class SampleComponent extends React.Component {
           <Row>
             <Col className={style['col1']} xs={6} md={3}>Hello, world!</Col>
             <Col className={style['col2']} xs={6} md={3}>Hello, world!</Col>
-            <Col className={style['col3']} xs={6} md={3}>Hello, world!</Col>
+            <Col className={style['col3']} xs={6} md={6}>Hello, world!</Col>
           </Row>
         </Grid>
       </div>
