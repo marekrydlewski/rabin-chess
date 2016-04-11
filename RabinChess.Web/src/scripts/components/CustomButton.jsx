54a637fcc1a9aff7bb9ce9eddcf066f9a304b10d
Marcin Jabłoński <jablon1994@gmail.com>
2016-04-11 19:18:35 +0200

    React layer of redux, problems

--- RabinChess.Web/src/scripts/components/CustomButton.jsx
+++ RabinChess.Web/src/scripts/components/CustomButton.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import Button from 'react-toolbox/lib/button'
 
-const CustomButton = () => (
-  <Button label="Hello world" raised accent />
+const CustomButton = (onClick, counter) => (
+  <Button label={counter} onClick={onClick} raised accent />
 )
+
+CustomButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  counter: PropTypes.number.isRequired
+}
 
 export default CustomButton
---
+++ RabinChess.Web/src/scripts/containers/CounterContainer.jsx
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux'
+import { increment } from '../actions/index.jsx'
+import CustomButton from '../components/CustomButton.jsx'
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onClick: () => {
+      dispatch(increment())
+    }
+  }
+}
+
+const CounterContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CustomButton)
+
+export default CounterContainer
