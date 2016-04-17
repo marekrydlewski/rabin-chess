1a29eba69a43474fba19372b4bc3780ee6aa9eeb
mpanowicz <michal.panowicz@student.put.poznan.pl>
2016-04-17 20:13:13 +0200

    Back to NavDrawer. Import Menu

--- RabinChess.Web/src/scripts/components/layout/Layout.jsx
+++ RabinChess.Web/src/scripts/components/layout/Layout.jsx
@@ -1,7 +1,8 @@
 import React, {PropTypes} from 'react'
 import {AppBar, Checkbox, IconButton, Button} from 'react-toolbox';
-import {Layout as RTLayout, Drawer, Panel, Sidebar} from 'react-toolbox';
+import {Layout as RTLayout, NavDrawer, Panel, Sidebar} from 'react-toolbox';
 import {Grid, Row, Col} from 'react-flexbox-grid'
+import Menu from './menu'
 
 import style from './main_layout';
 
@@ -27,39 +28,28 @@ class Layout extends React.Component {
     });
   }
 
-  goChessBoard() {
-    this.context.router.push('/routed');
-    this.setState({
-      drawerActive: false
-    });
-  }
-
   toggleDrawerActive() {
     this.setState({
       drawerActive: !this.state.drawerActive
     });
   };
 
-
   render() {
     return (
       <RTLayout className={style['main']}>
-        <Drawer active={this.state.drawerActive} pinned={this.state.drawerPinned} onOverlayClick={this.toggleDrawerActive}>
+        <NavDrawer active={this.state.drawerActive} pinned={this.state.drawerPinned} onOverlayClick={this.toggleDrawerActive}>
           <Grid className={style['menu']}>
-            <Row>
-              <Button className={style['menuItem']} onClick={this.goChessBoard.bind(this)}><p>Chessboard</p></Button>
-            </Row>
-            <Row>
-              <Button className={style['menuItem']}><p>Docs</p></Button>
-            </Row>
+          <Menu/>
           </Grid>
-        </Drawer>
+        </NavDrawer>
         <Panel>
           <AppBar className={style['header']}><IconButton icon='menu' inverse={true} onClick={this.toggleDrawerActive}/>
             <img className={style['logo']} src='./logo.png' onClick={this.goHome.bind(this)}/>
             <h1 onClick={this.goHome.bind(this)}>Rabin Chess</h1>
           </AppBar>
+          <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
             { this.props.children }
+          </div>
           <footer className={style['footer']}>
             <h5 className={style['text']}>Software Engineering Project</h5>
           </footer>
