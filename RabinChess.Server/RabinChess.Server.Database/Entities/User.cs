0eb09fde2d42f07a49f4ca0cde3595a0fd76e2a3
Marcin Jabłoński <jablon1994@gmail.com>
2016-04-24 12:33:18 +0200

    Initial database schema project

---
+++ RabinChess.Server/RabinChess.Server.Database/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RubinChess.Server.Database.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MinLength(5), MaxLength(25), Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public virtual List<Game> Games { get; set; } 
+    }
+}
