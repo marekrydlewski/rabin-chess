a15ea82a6b04412271faf541b447ecec183d7bfd
Marek Rydlewski <marek.rydlewski@outlook.com>
2016-04-29 21:58:27 +0200

    Proper imports

--- RabinChess.Web/src/scripts/components/index.jsx
+++ RabinChess.Web/src/scripts/components/index.jsx
@@ -1,6 +1,5 @@
 import App from './App'
 import Dashboard from './dashboard'
 import ChessboardLayout from './chessboard_layout'
-import Login from './login'
 
-export { App, ChessboardLayout, Dashboard, Login }
+export { App, ChessboardLayout, Dashboard }
