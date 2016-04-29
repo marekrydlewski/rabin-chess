d705a06b2f4bd58584b8d87bd9e55a0f98f2dfc1
Marek Rydlewski <marek.rydlewski@outlook.com>
2016-04-29 20:14:24 +0200

    A lot of stuff with ui

---
+++ RabinChess.Web/src/scripts/components/ui/Button/Button.jsx
@@ -0,0 +1,23 @@
+import React, {PropTypes, Component} from 'react';
+import classnames from 'classnames';
+
+import {Button as RButton} from 'react-toolbox';
+
+import style from './button.scss';
+
+export default class Button extends Component {
+    
+  static PropTypes = {
+    onChange: PropTypes.func,
+    label: PropTypes.string
+  }
+    
+  render() {
+    let { label, className, ...other } = this.props;
+    let buttonStyle = classnames(style['button'], className);
+    
+    return (
+      <RButton className={buttonStyle} {...other} label={label} raised/>
+    );
+  }
+};
