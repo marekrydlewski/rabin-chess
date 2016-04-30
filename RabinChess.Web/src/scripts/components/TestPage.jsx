4f4381cfb97ed9eb7073c4c4d503160b52e93f4f
Marcin Jabłoński <jablon1994@gmail.com>
2016-05-01 00:10:44 +0200

    added smart component for api interaction

--- RabinChess.Web/src/scripts/components/TestPage.jsx
+++ RabinChess.Web/src/scripts/components/TestPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import GameList from './game_list/GameList'
+import SmartGameList from './game_list'
 
 class TestPage extends React.Component {
   render(){
     return(
       <div>
-        <GameList games={[{title: 'Title 1', tags: 'Some tags 1', id: 'ID1'}, {title: 'Title 2', tags: 'Some tags 2', id: 'ID2'}]} />
+        <SmartGameList />
       </div>
     );
   }
