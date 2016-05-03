715ddce01afa2a4cb7cda6fba4c29a400d6907f3
Marcin Jabłoński <jablon1994@gmail.com>
2016-05-03 20:03:57 +0200

    Login first steps

---
+++ RabinChess.Server/RabinChess.Server.API/App_Start/Startup.Auth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.OAuth;
+using RabinChess.Server.API.Models;
+
+namespace RabinChess.Server.API
+{
+    public partial class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        public static string PublicClientId { get; private set; }
+
+        public static Func<UserManager<UserModel, int>> UserManagerFactory { get; private set; }
+ 
+        static Startup()
+        {
+            PublicClientId = "self";
+        }
+    }
+}
---
+++ RabinChess.Server/RabinChess.Server.API/Stores/UserStore.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using RabinChess.Server.API.Models;
+
+namespace RabinChess.Server.API.Stores
+{
+    public class UserStore : IUserPasswordStore<UserModel, int>
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public Task CreateAsync(UserModel user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync(UserModel user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(UserModel user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<UserModel> FindByIdAsync(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<UserModel> FindByNameAsync(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task SetPasswordHashAsync(UserModel user, string passwordHash)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<string> GetPasswordHashAsync(UserModel user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> HasPasswordAsync(UserModel user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
