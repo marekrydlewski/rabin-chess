f3607f1ab3132b321039d1a1209bfcbeff2d25fa
Marcin Jabłoński <jablon1994@gmail.com>
2016-05-03 18:35:44 +0200

    controller cleanup

--- RabinChess.Server/RabinChess.Server.API/Controllers/GamesController.cs
+++ RabinChess.Server/RabinChess.Server.API/Controllers/GamesController.cs
@@ -1,41 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using RabinChess.Server.API.Models;
 using RubinChess.Server.Logic;
 
 namespace RabinChess.Server.API.Controllers
 {
+    [Route("api/Games")]
     public class GamesController : ApiController
     {
-        // GET: api/Games
+        [Route("api/Games")]
+        [HttpGet]
         public List<GameListItemViewModel> Get()
         {
-            return ContextFactory.GetGamesContext().GetGames(0).Cast<GameListItemViewModel>().ToList();
-        }
-
-        // GET: api/Games/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Games
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT: api/Games/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/Games/5
-        public void Delete(int id)
-        {
+            return ContextFactory.GetGamesContext().GetGames(1).Cast<GameListItemViewModel>().ToList();
         }
     }
 }
