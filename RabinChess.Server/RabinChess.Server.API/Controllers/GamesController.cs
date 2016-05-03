e8e8ef877f8d0dfc56a4270eb99ce85e71013a91
Marcin Jabłoński <jablon1994@gmail.com>
2016-05-03 16:22:45 +0200

    Api controller

---
+++ RabinChess.Server/RabinChess.Server.API/Controllers/GamesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RabinChess.Server.API.Models;
+using RubinChess.Server.Logic;
+
+namespace RabinChess.Server.API.Controllers
+{
+    public class GamesController : ApiController
+    {
+        // GET: api/Games
+        public List<GameListItemViewModel> Get()
+        {
+            return ContextFactory.GetGamesContext().GetGames(0).Cast<GameListItemViewModel>().ToList();
+        }
+
+        // GET: api/Games/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Games
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Games/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Games/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
