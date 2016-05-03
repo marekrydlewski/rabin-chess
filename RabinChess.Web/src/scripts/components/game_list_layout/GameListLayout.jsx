1f2b2f6c759e1d25100a7227f710928fc42b7eb7
Marcin Jabłoński <jablon1994@gmail.com>
2016-05-03 14:47:05 +0200

    Grid magic

--- RabinChess.Web/src/scripts/components/game_list_layout/GameListLayout.jsx
+++ RabinChess.Web/src/scripts/components/game_list_layout/GameListLayout.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import {Card} from 'react-toolbox'
 import {Grid, Row, Col} from 'react-flexbox-grid'
 
+import style from './game_list_layout'
+
 import SmartGameList from '../game_list/SmartGameList'
 
 class GameListLayout extends React.Component {
   render() {
     return (
-      <Grid>
-        <Card>
-          <SmartGameList />
-        </Card>
+      <Grid className={style['game_list_grid']}>
+        <Row>
+          <Col sm md lg />
+          <Col xs={12} sm={10} md={10} lg={10}>
+            <Card>
+              <SmartGameList />
+            </Card>
+          </Col>
+          <Col sm md lg />
+        </Row>
       </Grid>
     )
   }
