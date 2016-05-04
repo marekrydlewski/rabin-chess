69d80cbb877008f7241302ba255942d12b8b37b0
K M <konr2303@gmail.com>
2016-05-04 20:05:27 +0200

    New Dashboard pics and playnow link

--- RabinChess.Web/src/scripts/components/dashboard/Dashboard.jsx
+++ RabinChess.Web/src/scripts/components/dashboard/Dashboard.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import {browserHistory} from 'react-router'
+import {Button, Link} from 'react-toolbox'
 import Slider from 'react-slick'
-import stonoga1 from '../../../assets/stonoga1.jpg'
-import stonoga2 from '../../../assets/stonoga2.jpg'
-import stonoga3 from '../../../assets/stonoga3.jpg'
-import stonoga4 from '../../../assets/stonoga4.jpg'
-import stonoga5 from '../../../assets/stonoga5.jpg'
-
+import andrzej from '../../../assets/andrzej.jpg'
+import stonoga2 from '../../../assets/rabin1.jpg'
+import stonoga3 from '../../../assets/rabin2.jpg'
+import stonoga4 from '../../../assets/rabin3.jpg'
+import stonoga5 from '../../../assets/rabin4.jpg'
+import playnow from '../../../assets/playnow.jpg'
 
 
 export default class Dashboard extends React.Component{
+
+static contextTypes= {
+    router: React.PropTypes.object.isRequired
+};
+
+constructor(props, context) {
+  super(props, context);
+}
+
+_onClick(){
+browserHistory.push('/board');
+}
+
   render() {
     let settings = {
       dots: true,
@@ -21,19 +36,27 @@ export default class Dashboard extends React.Component{
       autoplay: true,
       arrows: false,
       pauseOnHover: true,
-      autoplaySpeed: 2000,
+      autoplaySpeed: 2500,
       adaptiveHeight: true
     };
     let height=Math.floor(window.innerHeight * 0.4);
     return(
     <div >
+        <center>
+        <h3>Rabin Chess to super apka </h3>
+        <br/><br/>
         <Slider {...settings}>
-				    <div><center><img height={height} src={stonoga1}></img></center></div>
+				    <div><center><img height={height} src={andrzej}></img></center></div>
 				    <div><center><img height={height} src={stonoga2}></img></center></div>
 				    <div><center><img height={height} src={stonoga3}></img></center></div>
 				    <div><center><img height={height} src={stonoga4}></img></center></div>
 				    <div><center><img height={height} src={stonoga5}></img></center></div>
 		    </Slider>
+        <br/><br/>
+        <p>blablabla</p>
+        <br/><br/>
+        <a href="/#/board"><img height={height} src={playnow} /></a>
+        </center>
     </div>
 	);
   }
