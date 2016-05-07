619f8f5e6ed5e583c076c90302167b6b1a6112d8
Marek Rydlewski <marek.rydlewski@outlook.com>
2016-05-07 19:48:37 +0200

    Add full move component

---
+++ RabinChess.Web/src/scripts/components/chessboard/components/FullMove.jsx
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react'
+import { Chip } from 'react-toolbox'
+
+import Move from './Move'
+
+class FullMove extends React.Component {
+
+  static PropTypes = {
+    moveWhite: PropTypes.string,
+    moveBlack: PropTypes.string,
+    number: PropTypes.number
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+  let { moveWhite, moveBlack, number } = this.props;
+
+  return (
+      <div>
+        <MoveWithNumber move={ moveWhite } number={ number }/>
+        <Move move={ moveBlack }/>
+      </div>
+    )
+  }
+}
+
+export default FullMove
